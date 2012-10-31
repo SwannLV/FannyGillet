d4e98661a1effbbcb49a86cc5a827c4195c27f7f
SwannLV <le.vigouroux.swann@gmail.com>
2012-10-31 15:29:59 -0400

    update

--- Server.js
+++ Server.js
@@ -3,6 +3,5 @@ var HOST = process.env.IP;
 var PORT = process.env.PORT;
 
 fu.listen(Number(process.env.PORT || PORT), HOST);
-
 fu.get("/", fu.staticHandler("Index.html"));
 //fu.get("/THREEx.KeyboardState.js", fu.staticHandler("THREEx.KeyboardState.js"));
